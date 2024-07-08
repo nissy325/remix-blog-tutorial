de2e76aa2ec74b865e52317f04a60a7f27846827
nissy325 <240noadress+github@gmail.com>
2024-07-08 09:14:19 +0900

    add navigation

--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
@@ -39,6 +39,8 @@ export const action = async ({
     "title must be a string"
   );
 
+  // TODO: remove me
+  await new Promise((res) => setTimeout(res, 5000));
   await createPost({ title, slug, markdown})
 
   return redirect("/posts/admin");
@@ -48,6 +50,10 @@ const inputClassName = "w-full rounded border border-gray-500 px-2 py-1 text-lg"
 
 export default function NewPost() {
   const errors = useActionData<typeof action>();
+  const navigation = useNavigation();
+  const isCreating = Boolean(
+    navigation.state === 'submitting'
+  )
 
   return (
     <Form method="post">
@@ -98,8 +104,9 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
